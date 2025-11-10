a93a3d698bc85c7f0a41d9ecf98c39a5d5c0d36b
Jacob Wennerqvist <jawz_74@hotmail.com>
2025-11-11 00:00:12 +0100

    Fixat lite med testerna

--- LoopsAndStrings.Tests/StringUtilsTests.cs
+++ LoopsAndStrings.Tests/StringUtilsTests.cs
@@ -12,6 +12,7 @@ namespace LoopsAndStrings.Tests
         [Theory]
         [InlineData("Hej du gamle indian", 3, "gamle")]  // Korrekt input o output
         [InlineData("Tjabba   tjena       hallå", 3, "hallå")]  // Korrekt input o output (med många whitespace)
+        [InlineData("  Tjabba   tjena       hallå  ", 4, null)]  // Fel. Ord nr 4 saknas i strängen
         [InlineData("Ett två", 3, null)] // Fel, för få ord
         [InlineData("    ", 3, null)] // Fel, bara whitespace
         public void FindNthWordInSentence_ShouldReturnExpectedWord(string input, uint wordNo, string? expectedWord)
@@ -30,14 +31,19 @@ namespace LoopsAndStrings.Tests
         }
 
         [Theory]
-        [InlineData("Hej", true)]
-        [InlineData("", false)]
-        [InlineData("   ", false)]
-        [InlineData(null, false)]
-        public void IsStringValidText_ShouldValidateProperly(string? input, bool expected)
+        [InlineData("Hej", false, true)]  // Korrekt textsträng om tom/space ej tillåtna 
+        [InlineData("Hej", true, true)]  // Korrekt textsträng om tom/space tillåtna 
+        [InlineData("", false, false)]    // Korrekt textsträng om tom/space ej tillåtna
+        [InlineData("", true, true)]    // Korrekt textsträng om tom/space tillåtna
+        [InlineData("   ", false, false)] // Ej korrekt textsträng om tom/space ej tillåtna
+        [InlineData("   ", true, true)] // Korrekt textsträng om tom/space tillåtna        
+        [InlineData(null, false, false)] // Ej korrekt textsträng om tom/space ej tillåtna
+        [InlineData(null, true, false)] // Korrekt textsträng om tom/space tillåtna
+        public void IsStringValidText_ShouldValidateProperly(string? input, bool allowEmptyOrBlanksOnly, bool expected)
         {
-            var result = StringUtils.IsStringValidText(input);
+            var result = StringUtils.IsStringValidText(input, allowEmptyOrBlanksOnly);
             Assert.Equal(expected, result);
+
         }
 
         [Fact]
@@ -46,6 +52,11 @@ namespace LoopsAndStrings.Tests
             var result = StringUtils.RepeatText("hej", 3);
 
             Assert.Equal("1. hej, 2. hej, 3. hej", result);
+
+            result = StringUtils.RepeatText("hej", 0);
+
+            Assert.Equal("", result);
+            
         }
 
     }
